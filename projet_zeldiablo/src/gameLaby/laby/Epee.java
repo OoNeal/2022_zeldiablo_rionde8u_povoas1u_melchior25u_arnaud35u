8f8047e857bb12c1c1be751a20be0b28335ef809
POVOAS Florian <florian_povoas@icloud.com>
2022-05-30 11:37:26 +0200

    Ajout des calsses Epee et Potion

---
+++ projet_zeldiablo/src/gameLaby/laby/Epee.java
@@ -0,0 +1,12 @@
+package gameLaby.laby;
+
+public class Epee extends ObjetRamassable {
+
+    public Epee(int dx, int dy) {
+        super(dx, dy);
+    }
+
+    public String toString() {
+        return "C'est une epee";
+    }
+}
---
+++ projet_zeldiablo/src/gameLaby/laby/Potion.java
@@ -0,0 +1,12 @@
+package gameLaby.laby;
+
+public class Potion {
+
+    public Potion(int dx, int dy) {
+        super(dx, dy);
+    }
+
+    public String toString() {
+        return "C'est une potion";
+    }
+}
