cf378c0f1ac6114246a02a6d1174586dc9da46b9
Damien Melchior <damien.mel.2404@gmail.com>
2022-05-30 11:29:07 +0200

    Ajout et modification des classes pour l'inventaire

---
+++ projet_zeldiablo/src/gameLaby/laby/Position.java
@@ -0,0 +1,29 @@
+package gameLaby.laby;
+
+import java.util.Objects;
+
+public class Position {
+
+    private int x;
+    private int y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+}
