9a61013b05429b7ac523d5b4a12e70c08c84f065
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2022-05-31 11:52:31 +0200

    Modification de la classe ObjetRamassable

--- projet_zeldiablo/src/gameLaby/laby/ObjetRamassable.java
+++ projet_zeldiablo/src/gameLaby/laby/ObjetRamassable.java
@@ -2,9 +2,12 @@ package gameLaby.laby;
 
 public class ObjetRamassable extends Position{
 
-    private boolean equipe;
 
     public ObjetRamassable(int x, int y){
         super(x, y);
+    }
+
+    public int getDegats(){
+        return 0;
     }
 }
